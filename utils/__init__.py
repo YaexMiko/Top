583d6285eacc54d0fdbca4afe3232567d01086f1
Yae Miko <aloksinghmains756@gmail.com>
2025-04-02 23:00:22 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,12 +2,14 @@ from .helpers import (
     escape_markdown,
     get_correct_username,
     log_to_channel,
-    transform_mpd_links
+    transform_mpd_links,
+    get_safe_user_id
 )
 
 __all__ = [
     'escape_markdown',
     'get_correct_username',
     'log_to_channel',
-    'transform_mpd_links'
+    'transform_mpd_links',
+    'get_safe_user_id'
 ]
