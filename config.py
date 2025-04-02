acf02fc1d9b01301db7ede7e34f146316c54b62d
Yae Miko <aloksinghmains756@gmail.com>
2025-04-02 22:31:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # Bot Configuration
 API_ID = 27238809
 API_HASH = "c854867f7b27f65aebd41392eb2af1d9"
-BOT_TOKEN = "YOUR_MAIN_BOT_TOKEN"  # Replace with your actual token
+BOT_TOKEN = "7558079740:AAEjoMR5ykede9hFYhLioPMEhdjGTGMojZ8"  # Replace with your actual token
 
 # Logging Configuration
-LOG_CHANNEL_ID = -1002278484608  # Your log channel ID
+LOG_CHANNEL_ID = -1004728126086  # Your log channel ID
 
 # Other Settings
 MAX_FILE_SIZE = 20 * 1024 * 1024  # 20MB
